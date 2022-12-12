f5f4dc9fbddc7865ee8206a8ff00189bfc49798b
Yutao Gui <yutaogui@gmail.com>
2022-12-12 01:29:21 +0100

    Update solution template with boiler plate code to run as script

--- advent_of_code/aoc/templates/solution_template.py
+++ advent_of_code/aoc/templates/solution_template.py
@@ -1,4 +1,5 @@
 SOLUTION_TEMPLATE = """
+from pathlib import Path
 from typing import Union
 from advent_of_code.aoc.prepare_solution.base_solution import BaseSolution
 
@@ -9,4 +10,15 @@ class Solution(BaseSolution):
 
     def solve_part_two(self, problem_input: str) -> Union[str, int]:
         pass
+
+
+if __name__ == "__main__":
+    input_file = Path(__file__).parent / "problem_input.txt"
+
+    solution = Solution()
+    res = solution.solve_part_one(input_file.read_text())
+    print(res)
+
+    res = solution.solve_part_two(input_file.read_text())
+    print(res)
 """
