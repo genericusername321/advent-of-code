a3fce0067cb5cb175f47d948239bfda0dc33a7f5
Yutao Gui <yutaogui@gmail.com>
2022-12-12 01:27:54 +0100

    Delete test file in AOC2022D05

---
+++ advent_of_code/solutions/year_2022/day_07/solution.py
@@ -0,0 +1,11 @@
+
+from typing import Union
+from advent_of_code.aoc.prepare_solution.base_solution import BaseSolution
+
+
+class Solution(BaseSolution):
+    def solve_part_one(self, problem_input: str) -> Union[str, int]:
+        pass
+
+    def solve_part_two(self, problem_input: str) -> Union[str, int]:
+        pass
